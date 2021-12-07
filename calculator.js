8bf250dc16e196b1964a4fdd51a5942f6e8a3c31
thasup <thanachonfirst@hotmail.com>
2021-12-08 06:03:10 +0700

    Add post request

--- calculator.js
+++ calculator.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
 const port = 3000;
 
 app.listen(port, () => {
@@ -12,3 +14,11 @@ app.get('/', (req, res) => {
     // console.log(req);
     res.sendFile(`${__dirname}/index.html`);
 });
+
+app.post('/', (req, res) => {
+    let num1 = Number(req.body.n1);
+    let num2 = Number(req.body.n2);
+    let result = num1 + num2;
+    res.send(`The result of the calculation is ${result}`);
+});
+
