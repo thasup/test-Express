d815e646fd8a360a9cc8a5cb3a9bd670e6242748
thasup <thanachonfirst@hotmail.com>
2021-12-08 05:29:57 +0700

    Add calculator.js

--- calculator.js
+++ calculator.js
@@ -7,3 +7,8 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+    // console.log(req);
+    res.sendFile(`${__dirname}/index.html`);
+});
