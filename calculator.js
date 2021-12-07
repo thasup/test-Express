c7eca1e55ecb371b9e32be80fc20c839a08ce659
thasup <thanachonfirst@hotmail.com>
2021-12-08 06:14:22 +0700

    Feat BMI calculator

--- calculator.js
+++ calculator.js
@@ -15,10 +15,10 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
-app.post('/', (req, res) => {
-    let num1 = Number(req.body.n1);
-    let num2 = Number(req.body.n2);
-    let result = num1 + num2;
-    res.send(`The result of the calculation is ${result}`);
+app.post('/bmicalculator', (req, res) => {
+    let W = Number(req.body.weight);
+    let H = Number(req.body.height);
+    let result = W / (H * H);
+    res.send(`Your BMI is ${result}`);
 });
 
